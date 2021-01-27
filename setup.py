31f5c28adff1d760ff62d84c1c45beb31c7c66f8
Paul Stallard <pwastallard@googlemail.com>
2021-01-27 09:25:18 +0000

    Added strava_find

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     entry_points={
         'console_scripts': [
             'strava_summary=stravatools.summary:main',
+            'strava_find=stravatools.strava_find:main',
             'strava_get=stravatools.get_week:main',
             'strava_gridplot=stravatools.gridplot:main',
             'strava_csv_totals=stravatools.csv_totals:main',
---
+++ stravatools/strava_find.py
@@ -0,0 +1,124 @@
+import argparse
+import json
+import OSGridConverter as osg
+import polyline
+from math import radians, cos, sin, asin, sqrt
+from statistics import mean
+
+
+def polyline_bounding_box(pl: str, border: float = 0.0):
+    """Return the bounding box (latlon) of a polyline."""
+    latlon = polyline.decode(pl)
+    lats, lons = zip(*latlon)
+    return min(lats), min(lons), max(lats), max(lons)
+
+
+def bb_add_border(bb, border: float):
+    """Add a border of `border` km to all sides of bb."""
+    lat_border = border * 360/40075
+    mid_lat = mean([bb[0], bb[2]])
+    lon_border = lat_border / cos(radians(mid_lat))
+    return bb[0] - lat_border, bb[1] - lon_border, bb[2] + lat_border, bb[3] + lon_border
+
+
+def bb_add_point(bb, point):
+    """Extend bounding box (if necessary) to include point."""
+    return min(bb[0], point[0]), min(bb[1], point[1]), max(bb[2], point[0]), max(bb[3], point[1])
+
+
+def point_in_bounding_box(point, bb):
+    """Is point (lat, lon) in bounding box (min lat, min lon, max lat, max lon)"""
+    return bb[0] <= point[0] <= bb[2] and bb[1] <= point[1] <= bb[3]
+
+
+def haversine(lat1, lon1, lat2, lon2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+def find(point, radius, files, *, min_rad=0, activity_type=None, bb=False):
+    for f in files:
+        with open(f) as json_file:
+            data = json.load(json_file)
+        for act in reversed(data):
+            s = act["start_latlng"]
+            act_type = act["type"]
+            if not s or (activity_type and act_type != activity_type):
+                continue
+            if bb:
+                bounding_box = polyline_bounding_box(act["map"]["summary_polyline"])
+                bounding_box = bb_add_point(bounding_box, s)
+                bounding_box = bb_add_border(bounding_box, radius)
+                proximity = point_in_bounding_box(point, bounding_box)
+                d = 0
+            else:
+                d = haversine(*point, *s)
+                proximity = min_rad <= d < radius
+            if proximity:
+                url = f"https://www.strava.com/activities/{act['id']}"
+                print(f"{d:.02f} {url} - {act_type} {act['name']} - {act['start_date_local']}")
+
+
+def get_latlon(s):
+    bits = s.split(",")
+    lat = float(bits[0])
+    lon = float(bits[1])
+    return lat, lon
+
+
+def get_latlon_from_grid(s):
+    latlon = osg.grid2latlong(s)
+    return latlon.latitude, latlon.longitude
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Find activities near given location (distances in km)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-m", "--min", type=float, default=0.0, help="find activities at least this far from location")
+    parser.add_argument("-g", "--grid", action="store_true", default=False, help="use OS Grid Ref")
+    parser.add_argument(
+        "-b",
+        "--boundingbox",
+        action="store_true",
+        default=False,
+        help="use activity bounding box instead of start point",
+    )
+    parser.add_argument(
+        "-d", "--distance", type=float, default=1.0, help="find activities as most this far from location"
+    )
+    parser.add_argument(
+        "-a",
+        "--activity",
+        type=str,
+        choices=["Run", "Hike", "Bike"],
+        default=None,
+        help="only show activities of this type",
+    )
+    parser.add_argument("location", type=str, help="location of interest")
+    parser.add_argument("files", nargs="+", help="activity files")
+    args = parser.parse_args()
+
+    if args.grid:
+        ll = get_latlon_from_grid(args.location)
+    else:
+        ll = get_latlon(args.location)
+
+    find(ll, args.distance, args.files, min_rad=args.min, activity_type=args.activity, bb=args.boundingbox)
+
+
+if __name__ == "__main__":
+    main()
